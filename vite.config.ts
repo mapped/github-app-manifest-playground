a57f36ea654b384f5c0ea5b53b51c0399fc72c52
Henrique Lourenço <henrique@mapped.com>
2025-12-19 11:20:25 +0000

    fix: update Vite config to conditionally set base path based on command

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/github-app-manifest-playground/",
+  base: command === "build" ? "/github-app-manifest-playground/" : "/",
   build: {
     outDir: "dist",
   },
-});
+}));
